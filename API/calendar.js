9cd052640712b0cf2774a54a8e4f4556da3e4e18
misProyectosPropios <inakifrutos@outlook.com>
2024-07-29 12:03:16 -0300

    UPDATE ./API/calendar.js: DECLARATE globla variable: number_of_days_in_a_month

--- API/calendar.js
+++ API/calendar.js
@@ -1,3 +1,18 @@
+var number_of_days_in_a_month = {
+    1  : 31,
+    2  : 28, //Si no es año bisiesto
+    3  : 31,
+    4  : 30,
+    5  : 31,
+    6  : 30,
+    7  : 31,
+    8  : 31,
+    9  : 30,
+    10 : 31,
+    11 : 30,
+    12 : 31,
+  }
+
 function is_leap_year(year) {
-    
+
 }
