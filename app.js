1d5e4ce252109f68b6c276d809ac3fa55a7d2a5a
misProyectosPropios <inakifrutos@outlook.com>
2024-07-29 11:27:14 -0300

    MODIFY ./app.js: ADD middleware

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 const app = express()
 const port = 3000
 
+//Middleware
+app.use(body_parser.urlencoded({ extended: false })) //Para poder acceder a los parametros con POST, por medio del req.body[nombre_del-parametro]
+
+app.use(express.static('public')) //Para enviar el archivo js.js, el css y las imagines
+
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip + " - " + req.url)
+  console.log()
+  next()
+}) 
+
 app.get('/', (req, res) => {
     res.send('./views/month.html')
   })
-  
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
